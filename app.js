4974c070f17ec61c70e2a838801a55b382c9457c
See Loo Jane <muigek34@yahoo.com.sg>
2018-07-25 14:19:11 +0800

    add whitelist of origins for cors enabled requests

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
+const whitelist = [
+	"http://localhost:3001",
+	"https://week8-books-ui.herokuapp.com"
+];
+
+const corsOptions = {
+	origin: whitelist
+};
+
 const index = require("./routes/index");
 const books = require("./routes/books.js");
 const authors = require("./routes/authors.js");
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(express.json());
 
