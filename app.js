7b4bae0bc75957031b60e0579d511203175a4bf5
See Loo Jane <muigek34@yahoo.com.sg>
2018-07-25 15:55:41 +0800

    update cors to only allow heroku endpt if in prod mode

--- app.js
+++ app.js
@@ -2,27 +2,17 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-// const getOrigins = () => {
-// 	if (process.env.NODE_ENV === "production") {
-// 		console.log("is prod!");
-// 		return "https://week8-books-ui.herokuapp.com/";
-// 	} else {
-// 		console.log("is dev!");
-// 		return "*";
-// 	}
-// };
-
-const corsOptions = {
-	origin: ["http://localhost:3002", "https://week8-books-ui.herokuapp.com"]
-	// origin: getOrigins()
-};
-
 const index = require("./routes/index");
 const books = require("./routes/books.js");
 const authors = require("./routes/authors.js");
 
 const app = express();
-app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+	app.use({ origin: "https://week8-books-ui.herokuapp.com" });
+} else {
+	app.use(cors());
+}
 app.use(logger("dev"));
 app.use(express.json());
 
