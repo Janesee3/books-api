4f2790029e7e19ca6880f689580b79e183c53c52
See Loo Jane <muigek34@yahoo.com.sg>
2018-07-25 15:20:52 +0800

    revert cors to allow *

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
+// const getOrigins = () => {
+// 	if (process.env.NODE_ENV === "production") {
+// 		console.log("is prod!");
+// 		return "https://week8-books-ui.herokuapp.com/";
+// 	} else {
+// 		console.log("is dev!");
+// 		return "*";
+// 	}
+// };
+
 const corsOptions = {
-	origin: ["https://week8-books-ui.herokuapp.com/", "http://localhost:3001"]
+	// origin: ["http://localhost:3002", "https://week8-books-ui.herokuapp.com"]
+	origin: getOrigins()
 };
 
 const index = require("./routes/index");
@@ -11,7 +22,7 @@ const books = require("./routes/books.js");
 const authors = require("./routes/authors.js");
 
 const app = express();
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 
